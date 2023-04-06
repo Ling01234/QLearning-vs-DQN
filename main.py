f2fe344f08879501bd533235b8d5ae9b87c4edca
Ling Fei Zhang <lzhang133@gmail.com>
2023-04-06 15:57:22 -0400

    clean up, removed sac

--- main.py
+++ main.py
@@ -9,56 +9,6 @@ env_cartpole = gym.make("CartPole-v1")
 env_lunar_lander = gym.make("LunarLander-v2")
 
 
-def sac():
-    env = gym.make("InvertedPendulum-v4")
-    agent = Agent_SAC(input_dim=env.observation_space.shape, env=env,
-                      action_space=env.action_space.shape[0])
-
-    num_games = 1000
-    filename = "pendulum.png"
-    figure_file = "plots/" + filename
-
-    best_score = env.reward_range[0]
-    score_history = []
-    load_checkpoint = False
-
-    if load_checkpoint:
-        agent.load_models()
-        env.render(mode="human")
-
-    for i in trange(num_games):
-        obs, _ = env.reset()
-        done = False
-        score = 0
-        while not done:
-            action = agent.select_action(obs)
-            # print(f"action: {action}")
-
-            next_obs, reward, done, *_ = env.step(action)
-            score += reward
-            agent.remember(obs, action, reward, next_obs, done)
-
-            if not load_checkpoint:
-                agent.learn()
-
-            obs = next_obs
-
-        score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
-
-        if avg_score > best_score:
-            best_score = avg_score
-
-            if not load_checkpoint:
-                agent.save_models()
-
-        # print(f"episode {i} scores {score:.1f}, average score: {avg_score:1f}")
-
-    if not load_checkpoint:
-        x = [i+1 for i in range(num_games)]
-        plot(x, score_history, figure_file)
-
-
 def cartpole_dq():
     agent = Agent_DQ(alpha=0.003, gamma=0.99, env=env_cartpole, epsilon_start=1,
                      epsilon_decay=5e-4, epsilon_end=0.01, tau=0.005, batch_size=64)
--- replaybuffer.py
+++ replaybuffer.py
@@ -4,38 +4,6 @@ from itertools import count
 import random
 
 
-class Buffer():
-    def __init__(self, max_size, input_shape, action_space):
-        self.max_size = max_size
-        self.mem_counter = 0
-        self.state_memory = np.zeros((self.max_size, *input_shape))
-        self.new_state_memory = np.zeros((self.max_size, *input_shape))
-        self.action_memory = np.zeros((self.max_size, action_space))
-        self.reward_memory = np.zeros(self.max_size)
-        self.terminal = np.zeros(self.max_size, dtype=np.bool)
-
-    def remember(self, state, action, reward, new_state, done):
-        index = self.mem_counter % self.max_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = new_state
-        self.action_memory[index] = action
-        self.reward_memory[index] = reward
-        self.terminal[index] = done
-
-        self.mem_counter += 1
-
-    def sample_buffer(self, batch_size):
-        max_mem = min(self.mem_counter, self.max_size)
-        batch = np.random.choice(max_mem, batch_size)
-        states = self.state_memory[batch]
-        actions = self.action_memory[batch]
-        rewards = self.reward_memory[batch]
-        next_states = self.new_state_memory[batch]
-        dones = self.terminal[batch]
-
-        return states, actions, rewards, next_states, dones
-
-
 Transition = namedtuple("Transition",
                         ("state", "action", "reward", "next_state", "done"))
 
